375f490d910b2b54a42f42c8fb742e1bc0e7d0ff
RealPlay <ashbh@hotmail.co.uk>
2014-08-14 20:53:01 +0100

    trying this again

--- SHD/Assets/Scripts/XML/Character.cs
+++
@@ -1,5 +0,0 @@
-﻿using System.Collections;
-
-public class Character{
-	public string cid, cname = "INVALID CHARACTER", cimage, crace, cgender, cability, cbio, cabilityclass;
-}
--- SHD/Assets/Scripts/XML/CharacterContainer.cs
+++
@@ -1,8 +0,0 @@
-﻿using UnityEngine;
-using System.Collections;
-
-public class CharacterContainer{
-	public Texture iconTexture = new Texture();
-	public int id = -1;
-	public string powerClass = "";
-}
